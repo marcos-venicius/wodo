794170643c940d1c2c9123c9c8497e8497d29806
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-03-10 07:20:57 -0300

    remove unused code from visualizer.c

--- neovim-integration/visualizer.h
+++ neovim-integration/visualizer.h
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 #include "./systemtypes.h"
 
-void visualize_task(wodo_task_t task, bool full);
 void print_wodo_datetime(wodo_datetime_t timezoned_datetime, bool simple);
 
 #endif // !_WODO_VISUALIZER_H_
